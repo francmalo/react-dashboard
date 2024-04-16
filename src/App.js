f2df2ad476b50726ccbca73af4c73ef7abacabac
francmalo <frncsmalombe7328@gmail.com>
2024-04-16 12:03:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +15,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
   );
 }
