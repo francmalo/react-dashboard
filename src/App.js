03981a0db1083518d223c599f837f07759ae2de5
francmalo <frncsmalombe7328@gmail.com>
2024-04-22 11:41:43 +0300

    added theme.js for the colors and use mode of light/dark theme,also added topbar,sidebar and header too

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+import{ColorModeContext,useMode} from './theme'
+import {CssBaseline,ThemeProvider} from '@mui/material'
+import { Routes,Route } from 'react-router-dom';
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Contacts from "./scenes/contacts";
+// import Invoices from "./scenes/invoices";
+// import Form from "./scenes/form";
+
+
+
+
+
+
+
+
 
 function App() {
+  const[theme,colorMode]=useMode();
+
   return (
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
     <div className="app">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      
+    <Sidebar/>
+      <main className='content'>
+        <Topbar/>
+        
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+          {/* <Route path="/team" element={<Team/>}/>
+          <Route path="/contacts" element={<Contacts/>}/>
+          <Route path="/invoices" element={<Invoices/>}/>
+          <Route path="/form" element={<Form/>}/> */}
+
+        </Routes>
+      </main>
     </div>
+    </ThemeProvider>
+    
+  </ColorModeContext.Provider>
+   
   );
 }
 
